87d51eb1dcbd4db9acae8c8b80ba7191fddc2a88
= <=>
2023-12-06 00:12:10 +0700

    Init

---
+++ Assets/_Root/Scripts/Bot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour
+{
+    private void Awake()
+    {
+        var inputAsset = new InputAsset();
+    }
+}
